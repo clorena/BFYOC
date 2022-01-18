20a92f38b38ff49f1cb1f2d27e6d52cee2a0a606
Renjith R <renjithmails@gmail.com>
2022-01-18 21:18:38 +0530

    Fixing issues

--- FunctionApp2/Function1.cs
+++ FunctionApp2/Function1.cs
@@ -7,49 +7,59 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace FunctionApp2
+namespace FunctionApp1
 {
-    public static class WriteDoc
+    public static class Products
     {
-        [FunctionName("CreateUser")]       
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] User[] users,
-            [CosmosDB(
-                databaseName: "UserProducts",
-                collectionName: "Users",
-                ConnectionStringSetting = "AzureWebJobsStorage")]IAsyncCollector<User> userItemsOut,
-            ILogger log)
-        {
-            log.LogInformation($"C# Queue trigger function processed {users?.Length} items");
+        //    [FunctionName("GetProduct")]
+        //    public static async Task<IActionResult> Run(
+        //        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        //        ILogger log)
+        //    {
+        //        log.LogInformation("C# HTTP trigger function processed a request.");
 
-            foreach (User user in users)
-            {
-                log.LogInformation($"Description={user.UserName}");
-                await userItemsOut.AddAsync(user);
-            }
-            return new OkObjectResult(true);
-        }
-        
+        //        string productId = req.Query["productId"];            
+        //        var products = new List<Product>() 
+        //        { 
+        //            new Product() { Id="1", Name="Prod1" },
+        //            new Product() { Id="2", Name="Prod2"
+        //        }};
+        //        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        //        dynamic data = JsonConvert.DeserializeObject(requestBody);
+        //        productId = productId ?? data?.productId;
+        //        string responseMessage = string.Empty;
+        //        var product = products.Where(x => x.Id == productId).FirstOrDefault();
+        //        if (product != null)
+        //        {                
+        //            responseMessage = product.Name;
+        //        }
+        //        return new OkObjectResult(responseMessage);
+        //    }
+        //}
 
         [FunctionName("CreateProduct")]
-        public static async Task<IActionResult> CreateProduct(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
-            [CosmosDB(
-                databaseName: "UserProducts",
+        public static async Task<IActionResult> Run(
+                [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+                [CosmosDB(databaseName: "ToDoItems",
                 collectionName: "Products",
-                ConnectionStringSetting = "AzureWebJobsStorage")]IAsyncCollector<Product> productItemsOut,
-            ILogger log)
+                CreateIfNotExists = true,
+                ConnectionStringSetting = "AzureWebJobsStorage")] IAsyncCollector<Product> productItemsOut,
+                ILogger log)
         {
-            var products = new Product[] { };
-            log.LogInformation($"C# Queue trigger function processed {products?.Length} items");
-            
-            foreach (Product product in products)
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(requestBody);
+            log.LogInformation($"C# Queue trigger function processed {products?.Count()} items");
+
+            foreach (var product in products)
             {
                 log.LogInformation($"Description={product.ProductName}");
                 await productItemsOut.AddAsync(product);
             }
             return new OkObjectResult(true);
         }
+
     }
 }
--- FunctionApp2/TableEntities.cs
+++ FunctionApp2/TableEntities.cs
@@ -1,19 +1,20 @@
-﻿using Microsoft.Azure.Cosmos.Table;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace FunctionApp2
+namespace FunctionApp1
 {
     public class User
     {
         public string UserId { get; set; }
         public string UserName { get; set; }
-        public string FullName { get; set; }        
+        public string FullName { get; set; }
     }
 
     public class Product
     {
         public string ProductId { get; set; }
         public string ProductName { get; set; }
-        public string ProductDescription { get; set; }       
+        public string ProductDescription { get; set; }
     }
 }
