66c9ce7486d6fa7ae1238f3646b2d2c8f531ecf7
Renjith R <renjithmails@gmail.com>
2022-01-19 07:43:23 +0530

    Final check-in

--- FunctionApp2/Function1.cs
+++ FunctionApp2/Function1.cs
@@ -40,10 +40,30 @@ namespace FunctionApp1
         //    }
         //}
 
-        [FunctionName("CreateProduct")]
-        public static async Task<IActionResult> Run(
+        [FunctionName("CreateUser")]
+        public static async Task<IActionResult> CreateUser(
                 [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
-                [CosmosDB(databaseName: "ToDoItems",
+                [CosmosDB(databaseName: "UserProduct",
+                collectionName: "Users",
+                CreateIfNotExists = true,
+                ConnectionStringSetting = "AzureWebJobsStorage")] IAsyncCollector<User> productItemsOut,
+                ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var users = JsonConvert.DeserializeObject<IEnumerable<User>>(requestBody);
+            log.LogInformation($"C# Queue trigger function processed {users?.Count()} items");
+
+            foreach (var user in users)
+            {
+                await productItemsOut.AddAsync(user);
+            }
+            return new OkObjectResult(true);
+        }
+
+        [FunctionName("CreateProduct")]
+        public static async Task<IActionResult> CreateProduct(
+                [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+                [CosmosDB(databaseName: "UserProduct",
                 collectionName: "Products",
                 CreateIfNotExists = true,
                 ConnectionStringSetting = "AzureWebJobsStorage")] IAsyncCollector<Product> productItemsOut,
@@ -61,5 +81,73 @@ namespace FunctionApp1
             return new OkObjectResult(true);
         }
 
+        [FunctionName("GetProducts")]
+        public static async Task<IActionResult> GetProducts(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "products")] HttpRequest req,
+            [CosmosDB(databaseName: "UserProduct", collectionName: "Products", SqlQuery = "SELECT * FROM Products",
+            ConnectionStringSetting = "AzureWebJobsStorage")] IEnumerable<Product> products,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (products is null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(products);
+        }
+
+        [FunctionName("GetProduct")]
+        public static async Task<IActionResult> GetProduct(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "product/{id}")] HttpRequest req,
+            [CosmosDB(databaseName: "UserProduct", collectionName: "Products", SqlQuery = "SELECT * FROM Products p where p.ProductId = {id}",
+            ConnectionStringSetting = "AzureWebJobsStorage")] IEnumerable<Product> products,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (products is null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(products);
+        }
+
+        [FunctionName("GetUsers")]
+        public static async Task<IActionResult> GetUsers(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "users")] HttpRequest req,
+            [CosmosDB(databaseName: "UserProduct", collectionName: "Users", SqlQuery = "SELECT * FROM Users",
+            ConnectionStringSetting = "AzureWebJobsStorage")] IEnumerable<User> users,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (users is null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(users);
+        }
+
+        [FunctionName("GetUser")]
+        public static async Task<IActionResult> GetUser(
+                [HttpTrigger(AuthorizationLevel.Function, "get", Route = "user/{id}")] HttpRequest req,
+                [CosmosDB(databaseName: "UserProduct", collectionName: "Users", SqlQuery = "SELECT * FROM Users p where p.UserId = {id}",
+            ConnectionStringSetting = "AzureWebJobsStorage")] IEnumerable<User> users,
+                ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (users is null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(users);
+        }
     }
 }
+
